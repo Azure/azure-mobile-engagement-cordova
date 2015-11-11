950be577f273cbbf4c6073092aaea163904aec59
Olivier Goguel <ogoguel@MacBook-Pro-de-Olivier.local>
2015-11-11 22:25:50 +0100

    fix typo

--- hooks/after_plugin_install.js
+++ hooks/after_plugin_install.js
@@ -2,7 +2,7 @@
 module.exports = function(context) {
 	
 	var cmd = context.cmdLine.toLowerCase();
-	var found = cmd.match(/--variable enablelocation=(\S*)/);
+	var found = cmd.match(/--variable\s*enablelocation=(\S*)/);
 	if (!found) 
 		return ;
 	var location = found[1];
@@ -12,7 +12,7 @@ module.exports = function(context) {
 	var pluginDir = context.opts.plugin.pluginInfo.dir + "#:"+location+"-location";
 	context.cordova.raw.plugin('add', pluginDir );
 
-	found = cmd.match(/--variable backgroundreporting=(\S*)/);
+	found = cmd.match(/--variable\s*backgroundreporting=(\S*)/);
 	var background = (found && found[1]=="true")?"background":"foreground";
 	
 	pluginDir = context.opts.plugin.pluginInfo.dir + "#:"+background+"-reporting";
