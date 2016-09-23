122777b75371cf0eb82c65838429ade6f0e59fa8
Alexander Sorokin <alexander.sorokin@akvelon.com>
2016-09-23 16:41:17 +0300

    Fixed an error when adding the plugin with --variable enableLocation=lazyarea

--- hooks/after_plugin_install.js
+++ hooks/after_plugin_install.js
@@ -2,20 +2,20 @@
  * Copyright (c) Microsoft Corporation.  All rights reserved.
  * Licensed under the MIT license. See License.txt in the project root for license information.
  */
- 
+
 azmeLocationExtensionAlreadyInstalled = false;
 
 module.exports = function(context) {
-	
+
 	// Do not install for each platform!
 	if (azmeLocationExtensionAlreadyInstalled == true)
 		return ;
 	azmeLocationExtensionAlreadyInstalled = true;
-	
-	var cmd = context.cmdLine;	
+
+	var cmd = context.cmdLine;
 	var regex = /--variable\s*(\w*)=([\S]*)/g
 	var variables = {};
-	var opts = { 
+	var opts = {
 			cli_variables:{}
 		};
 	var match = regex.exec(cmd);
@@ -26,13 +26,13 @@ module.exports = function(context) {
 	    variables[key.toLowerCase()]=value // keep variables in lowercase to simplify detection
 	    match = regex.exec(cmd);
 	}
-	
+
 	var location = variables.enablelocation;
 	if (!location)
 		return ;
 
 	location = location.toLowerCase();
-	if (location != "lazyarera" && location != "realtime" && location != "finerealtime")
+	if (location != "lazyarea" && location != "realtime" && location != "finerealtime")
 		throw new Error( "Unsupported parameter with --variable enableLocation: "+location);
 
 	var locationPluginDir = context.opts.plugin.pluginInfo.dir + "#:"+location+"-location";
@@ -43,6 +43,6 @@ module.exports = function(context) {
 		var reporting = variables.backgroundreporting;
 		var background = (reporting && reporting.toLowerCase()=="true")?"background":"foreground";
 		var reportingPluginDir = context.opts.plugin.pluginInfo.dir + "#:"+background+"-reporting";
-		context.cordova.raw.plugin('add', reportingPluginDir , opts);		
+		context.cordova.raw.plugin('add', reportingPluginDir , opts);
 	});
 }
