1fd1632c37abc21ff65879ce10674b06b5103945
Olivier Goguel <ogoguel@MacBook-Pro-de-Olivier.local>
2016-10-05 22:46:56 +0200

    Updated Android SDK to 4.2.3

--- src/android/cordova/AZME.java
+++ src/android/cordova/AZME.java
@@ -30,7 +30,7 @@ public class AZME extends CordovaPlugin {
 
     private static final String pluginName = "CDVAZME";
     private static final String pluginVersion = "3.2.1";
-    private static final String nativeSDKVersion = "4.1.0"; // to eventually retrieve from the SDK itself
+    private static final String nativeSDKVersion = "4.2.3"; // to eventually retrieve from the SDK itself
 
     public CordovaInterface cordova = null;
     public CordovaWebView webView = null;
